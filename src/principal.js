ca50c011e6b5a0e768b48f75dce8f8d4c4404dd6
Maciel Sousa <fmacielsousa@gmail.com>
2021-05-12 00:10:13 -0300

    Adicionando estilo na DOM

--- src/principal.js
+++ src/principal.js
@@ -1,5 +1,6 @@
 import Pessoa from './pessoa'
 import './modulo/moduloA'
+import './assets/css/estilo.css'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimnetar())
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
      output: {
           filename: 'principal.js',
           path: __dirname + '/public'
+     },
+     module: {
+          rules: [{
+               test: /\.css$/,
+               use: [
+                    'style-loader', //Reponsavel por adicionar dentro da DOM o CSS injetando a tag <style>
+                    'css-loader' //interpretatr @import, url()...
+               ]
+          }]
      }
 }
